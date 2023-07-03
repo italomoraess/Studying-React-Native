ac3b6045af954588f33b26906f213729f27ca04c
italomoraess <italomoraes.sth@gmail.com>
2023-07-03 17:39:41 -0300

    Conditional rendering and intup text

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground, Button} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground, Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+
   const handleLoginButton = () => {
-    return alert('Login!!')
+    setName('Italo Moraes');
+    setAge(26);
+  }
+
+  const handleClear = () => {
+    setName('');
+    setAge(0)
   }
 
   return (
     <View style={styles.container}>
-      <Text style={{color: 'FFF'}}>My First App</Text>
-      <StatusBar style="auto" />
       <Image
         source={{uri: 'hhtps://www.google.com.br/google.jpg'}} 
         style={{width: 300, height: 100}}      
       />
+      <TextInput
+       style={{backgroundColor: '#EEE', padding: 10}}
+       placeholder='Digite seu nome:'
+      />
+
+      { name != '' && <Text>Logado com {name} e tem {age} anos</Text> }
+
+      { name != '' && <Button title='Limpar Tudo' onPress={handleClear} /> }
+
       <Button title='Login' onPress={() => {handleLoginButton()}} />
     </View>
   );
