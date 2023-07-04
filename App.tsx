297a1ed4f4876471f8bdef63070b82127de595ff
italomoraess <italomoraes.sth@gmail.com>
2023-07-04 00:09:55 -0300

    Conditional expression

--- App.tsx
+++ App.tsx
@@ -3,39 +3,44 @@ import { StyleSheet, Text, View, Image, ImageBackground, Button, TextInput } fro
 
 export default function App() {
   const [name, setName] = useState('');
-  const [age, setAge] = useState(0);
+  const [nameField, setNameField] = useState('');
 
-  const handleLoginButton = () => {
-    setAge(26);
+  const handleCadastroButton = () => {
+    setName(nameField);
   }
 
   const handleClear = () => {
     setName('');
-    setAge(0)
   }
 
   return (
     <View style={styles.container}>
-      
-      <TextInput
-        style={{ backgroundColor: '#EEE', padding: 10 }}
-        placeholder='Digite seu nome'
-        placeholderTextColor={'#F4793B'}
-        value={name}
-        onChangeText={text => setName(text)}
-      />
+      {name == '' &&
+      <>
+        <TextInput
+          style={{ backgroundColor: '#EEE', padding: 10 }}
+          placeholder='Digite seu nome'
+          placeholderTextColor={'#F4793B'}
+          value={nameField}
+          onChangeText={text => setNameField(text)}
+        />
 
-      {name != '' && <Text>Logado com {name} e tem {age} anos</Text>}
+        <Button title='Cadastrar' onPress={() => { handleCadastroButton() }} /> 
+      </>
+      }
 
-      {name != '' && <Button title='Limpar Tudo' onPress={handleClear} />}
-
-      <Button title='Cadastrar' onPress={() => { handleLoginButton() }} />
+      {name != '' && 
+        <>
+          <Text>Cadastrado com sucesso: {name}</Text>
+          <Button title='Cadastrar Outro' onPress={handleClear} />
+        </>
+      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    paddingTop: 50,
   },
 });
